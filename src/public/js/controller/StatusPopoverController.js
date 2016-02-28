63fb23069481ddc7d0b553bb603a3d132ab7fd67
yu-zhengyu <yuzheng@andrew.cmu.edu>
2016-02-28 15:33:44 -0500

    update status change and add controller

---
+++ src/public/js/controller/StatusPopoverController.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Yu Zheng on 2016/2/27.
+ */
+MyApp.angular.controller('StatusPopoverController',
+    ['$scope', '$http', '$window', '$location', 'BootService',
+    function ($scope, $http, $window, $location, BootService) {
+
+        var fw7 = MyApp.fw7.app;
+        var $$ = Dom7;
+        $scope.select = function(status) {
+        	$$('.navbar').find('a.link.open-popover').text(status);
+        	fw7.closeModal();
+        	fw7.alert("Your status: " + status, "Status Changed");
+        }
+
+        BootService.addEventListener('login', function() {
+            $scope.username = MyApp.username;
+            $http.get("/api/users").success(function(users) {
+                $scope.users = users;
+                UserService.addUsers(users);
+            });
+        });
+    }
+]);
